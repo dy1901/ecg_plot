13174d7bdc7551970ac64274ca72f1d81dd20986
Yue <dy1901@gmail.com>
2019-05-14 00:18:56 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# ecg_plot
+# ECG plot
+
+![example 12 lead plot](/example.png)
+
+## Plot standard ECG chart from data.
+* Support both direct plotting and plotting SVG preview in browser (currently only works on mac)
+* Support saving PNG and SVG to disk
+
+## Notice
+* Input data should be m x n matrix, which m is lead count of EGC and n is length of single lead signal.
+* Default sample rate is 500 Hz.
+
+## Example
+
+#### Plot 12 lead ECG 
+
+```
+import ecg_plot
+
+ecg = load_data() # load data should be implemented by yourself 
+ecg_plot.plot_12(ecg, sample_rate = 500, title = 'ECG 12')
+ecg_plot.show()
+
+```
+
+#### Plot single lead ECG
+
+```
+import ecg_plot
+
+ecg = load_data() # load data should be implemented by yourself 
+ecg_plot.plot_1(ecg[1], sample_rate=500, title = 'ECG'):
+ecg_plot.show()
+```
+
+### License: MIT
---
+++ ecg_plot.py
@@ -0,0 +1,108 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
+import os
+from math import ceil 
+
+
+def _ax_plot(ax, x, y):
+    ax.set_xticks(np.arange(0,11,0.2))    
+    ax.set_yticks(np.arange(-2,3,0.5))
+
+    ax.minorticks_on()
+    
+    ax.xaxis.set_minor_locator(AutoMinorLocator(5))
+
+    ax.grid(which='major', linestyle='-', linewidth='0.5', color='red')
+    ax.grid(which='minor', linestyle='-', linewidth='0.5', color=(1, 0.7, 0.7))
+   
+    ax.set_ylim(-1.8, 1.8)
+    ax.set_xlim(0, 10)
+
+    ax.plot(x,y, linewidth=0.5)
+
+
+lead_index = ['I', 'II', 'III', 'aVR', 'aVL', 'aVF', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6']
+def plot_12(
+        ecg, 
+        sample_rate = 500, 
+        title       = 'ECG 12', 
+        lead_index  = lead_index, 
+        lead_order  = None,
+        columns     = 2
+        ):
+    if not lead_order:
+        lead_order = list(range(0,len(ecg)))
+
+    leads = len(lead_order)
+    seconds = len(ecg[0])/sample_rate
+
+    plt.rcParams.update({'font.size': 8})
+    fig, ax = plt.subplots(
+        ceil(len(lead_order)/columns),columns,
+        sharex=True, 
+        sharey=True,
+        figsize=(0.7*seconds*columns, 1.1*leads/columns)
+        )
+    fig.subplots_adjust(
+        hspace = 0, 
+        wspace = 0.04,
+        left   = 0.04,  # the left side of the subplots of the figure
+        right  = 0.98,  # the right side of the subplots of the figure
+        bottom = 0.06,  # the bottom of the subplots of the figure
+        top    = 0.95
+        )
+    fig.suptitle(title)
+
+    for i in range(0, len(lead_order)):
+        if(columns == 1):
+            t_ax = ax[i]
+        else:
+            t_ax = ax[i//columns,i%columns]
+        t_lead = lead_order[i]
+        t_ax.set_ylabel(lead_index[t_lead])
+        t_ax.tick_params(axis='x',labelrotation=90)
+        step = 1.0/sample_rate
+        _ax_plot(t_ax, np.arange(0, len(ecg[t_lead])*step, step), ecg[t_lead])
+        
+
+def plot_1(ecg, sample_rate=500, title = 'ECG'):
+    plt.figure(figsize=(15,2))
+    plt.suptitle(title)
+    plt.subplots_adjust(
+        hspace = 0, 
+        wspace = 0.04,
+        left   = 0.04,  # the left side of the subplots of the figure
+        right  = 0.98,    # the right side of the subplots of the figure
+        bottom = 0.2,   # the bottom of the subplots of the figure
+        top    = 0.88
+        )
+
+    ax = plt.subplot(1, 1, 1)
+    step = 1.0/sample_rate
+    _ax_plot(ax,np.arange(0,len(ecg)*step,step),ecg)
+    
+DEFAULT_PATH = './'
+show_counter = 1
+def show_svg():
+    global show_counter
+    file_name = DEFAULT_PATH + "show_tmp_file_{}.svg".format(show_counter)
+    plt.savefig(file_name)
+    os.system("open {}".format(file_name))
+    show_counter += 1
+    plt.close()
+
+def show():
+    plt.show()
+
+
+def save_as_png(file_name, path = DEFAULT_PATH):
+    plt.ioff()
+    plt.savefig(path + file_name + '.png')
+    plt.close()
+
+def save_as_svg(file_name, path = DEFAULT_PATH):
+    plt.ioff()
+    plt.savefig(path + file_name + '.svg')
+    plt.close()
+
